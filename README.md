b5fdcce64cb0baaaf981f41cfd761a9296ea51b8
james4-daf <d.james@elsevier.com>
2022-11-17 22:03:04 +0000

    keeper app

---
+++ README.md
@@ -0,0 +1,9 @@
+# Keeper App
+
+## To run
+
+```
+npm start
+```
+
+Google keep replica
---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import notes from "../notes";
+import Footer from "./Footer";
+import Header from "./Header";
+import Note from "./Note";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map((note) => (
+        <Note key={note.id} title={note.title} content={note.content} />
+      ))}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
